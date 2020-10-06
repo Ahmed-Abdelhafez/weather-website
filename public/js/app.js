07288c6ab284f54256817b516200cebd299a3858
Ahmed Emad <a.emad23545@gmail.com>
2020-10-06 09:42:16 +0200

    setup for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ locationform.addEventListener('submit', (e) => {
     const location= search.value
     massageone.textContent = 'loading...'
     massagetwo.textContent = ''
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
     response.json().then((data) => {
         if(data.error){
             massageone.textContent = data.error
--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const hbs = require('hbs')
 const { error } = require('console')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -81,6 +82,6 @@ app.get('/weather', (req, res) => {
             })
         })
 
-        app.listen(3000, () => {
-            console.log('Server is up on port 3000.')
+        app.listen(port, () => {
+            console.log('Server is up on port ' + port)
         })
